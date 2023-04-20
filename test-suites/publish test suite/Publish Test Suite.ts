6738bccb694ce42d807bfe7885bc9aaf4a20b2cf
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-20 17:35:13 +0700

    update

--- test-suites/publish test suite/Publish Test Suite.ts
+++ test-suites/publish test suite/Publish Test Suite.ts
@@ -1,13 +1,21 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>Publish Test Suite</name>
-    <testSuiteGuid>2f7e660c-55fa-4e1c-9839-e041a92e4c09</testSuiteGuid>
+    <testSuiteGuid>29bcef52-7d8b-4b39-a214-531a1ec946ae</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
-        <guid>88126669-6804-422d-98c6-13e984c0ead6</guid>
+        <guid>5d0f61f1-0eb7-44a0-b235-efc39eeca721</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
-        <guid>d19971bb-0cb7-41cf-bfd8-1c97e13f265e</guid>
+        <guid>35387aee-8af3-482a-975c-9dfe714d1379</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/New test case</testCaseId>
+        <guid>44097229-c107-4217-97c0-09c5f73c0193</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
+        <guid>cf2bf519-4405-44bb-80bd-da32b44480c0</guid>
     </testCaseLink>
 </TestSuiteEntity>
