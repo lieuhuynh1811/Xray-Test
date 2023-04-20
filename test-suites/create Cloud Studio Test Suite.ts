d857c576b2e85157809b3246e9e8d70b3d78da69
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-20 17:37:27 +0700

    update

--- test-suites/create Cloud Studio Test Suite.ts
+++ test-suites/create Cloud Studio Test Suite.ts
@@ -1,9 +1,13 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>create Cloud Studio Test Suite</name>
-    <testSuiteGuid>45a7d640-b535-4a2c-aa66-481606d21782</testSuiteGuid>
+    <testSuiteGuid>92c183c9-d99b-4e13-85fd-900ba33bc537</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/Folder1/New test case</testCaseId>
-        <guid>5d5712f3-8c76-4e1c-8292-8044374a8e0b</guid>
+        <guid>6c1e259f-8296-44a5-9f81-f24ca6e54366</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case 1</testCaseId>
+        <guid>4cf5d37a-182f-4626-b243-f9775c232bcb</guid>
     </testCaseLink>
 </TestSuiteEntity>
