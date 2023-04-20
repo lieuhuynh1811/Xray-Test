0eff5193371e2385af55b90e35f2a3285aea6fb9
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-20 17:26:50 +0700

    first commit

---
+++ test-suites/publish test suite/Publish Test Suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Publish Test Suite</name>
+    <testSuiteGuid>2f7e660c-55fa-4e1c-9839-e041a92e4c09</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
+        <guid>88126669-6804-422d-98c6-13e984c0ead6</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
+        <guid>d19971bb-0cb7-41cf-bfd8-1c97e13f265e</guid>
+    </testCaseLink>
+</TestSuiteEntity>
