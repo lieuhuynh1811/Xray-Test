55e49ad5b2d813fa1dc61bb38e1ce1cc9ce6167a
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-05-24 14:01:53 +0700

    leo

---
+++ test-suites/publish test suite/leoleo.ts
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>leoleo</name>
+    <testSuiteGuid>03620029-cb4b-4873-a988-8cd8abcf45ca</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Folder3000/Test Case</testCaseId>
+        <guid>cb2dffa2-1aac-4f48-85d6-b65a1a29ccce</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
+        <guid>acd0bf8e-ff9b-4141-9871-535b51c02a26</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case 1</testCaseId>
+        <guid>1a226bed-1b86-4947-bfc2-41c55d63654e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Test Case 1</testCaseId>
+        <guid>e065b7ec-3750-4bcd-9b37-7316010ca64b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
+        <guid>c451dbf8-e5bd-43df-9685-b90ca320bf1c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/New test case</testCaseId>
+        <guid>01bb7cd8-bf0c-4ea8-b138-7455ec8b1d81</guid>
+    </testCaseLink>
+</TestSuiteEntity>
