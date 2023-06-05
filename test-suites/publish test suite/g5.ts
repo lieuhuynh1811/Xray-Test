5f7e383340040d0b1e186c79b1df5b44e958f789
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-06-05 16:29:03 +0700

    commit

---
+++ test-suites/publish test suite/g5.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>g5</name>
+    <testSuiteGuid>dfedb5bc-bfa8-4e34-b725-039aad81e910</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Folder3000/Test Case</testCaseId>
+        <guid>710a3237-b408-4c78-91f9-710510e5b3f5</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
+        <guid>5887d238-7231-4cfe-bbb5-4c8e55734e7a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case 1</testCaseId>
+        <guid>ebbc0b51-d6c4-4d1f-8404-355cacd0709b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Test Case 1</testCaseId>
+        <guid>f96be582-435b-45df-9053-fdc175bbce90</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
+        <guid>0248f82f-e991-463d-96b9-a4cffd995520</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/New test case</testCaseId>
+        <guid>aa05b4f4-5101-45f9-9120-138784703cae</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/leoleo</testCaseId>
+        <guid>98e999bd-b635-4d2d-954d-c01a0954003e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/g5 draft</testCaseId>
+        <guid>3f92c5cf-6e25-420b-950b-efcd38bdd26a</guid>
+    </testCaseLink>
+</TestSuiteEntity>
