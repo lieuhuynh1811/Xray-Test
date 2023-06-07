13a0f563b0bd423c8d01332347091395c44f22fb
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-06-07 17:28:31 +0700

    cm

---
+++ test-suites/G5.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>G5</name>
+    <testSuiteGuid>a88ea18f-b0dc-439f-b511-7cd55852a01f</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/g5</testCaseId>
+        <guid>793498e7-1310-4ba9-9aa0-0d79a2e64d4d</guid>
+    </testCaseLink>
+</TestSuiteEntity>
