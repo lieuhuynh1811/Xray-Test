5b07dc57d14e790705a69a0450566d22eb6d30ea
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-06-20 16:39:48 +0700

    first commit

---
+++ test-suites/regression-20062023/G5.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>G5</name>
+    <testSuiteGuid>4b672500-cb4c-4672-962b-af90fff833bc</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Folder3000/Test Case</testCaseId>
+        <guid>83009da7-b15c-4d4a-b22d-40cdfc6314f1</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
+        <guid>f6fb82d5-fc8a-4533-aa1b-efef0f04c3ef</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case 1</testCaseId>
+        <guid>84f06379-1d83-406c-b635-117feb7412bd</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Test Case 1</testCaseId>
+        <guid>0982f7e0-3eea-46fd-83b4-f40082f902c2</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
+        <guid>eed76741-521b-4bb1-acf8-ca6d3d5cf82b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/New test case</testCaseId>
+        <guid>ec3b041b-23aa-42d7-9a34-185c935ba333</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/leoleo</testCaseId>
+        <guid>2d0ab426-beb0-45ce-b6b8-303827aabc33</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/g5</testCaseId>
+        <guid>b9cddc20-0fd1-4a35-a518-9eaa1df30710</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/g5 draft</testCaseId>
+        <guid>b3deb1b1-4d79-48e4-8c87-716026aa130f</guid>
+    </testCaseLink>
+</TestSuiteEntity>
