375cf0f1bf374cbb4bebfe5d9912af3a7e1c4fed
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-07-11 10:08:39 +0700

    Katalon: Update test suite Cloud Studio Suite 1 in test-suites/xray

--- test-suites/xray/Cloud Studio Suite 1.ts
+++ test-suites/xray/Cloud Studio Suite 1.ts
@@ -1,41 +1,77 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>Cloud Studio Suite 1</name>
-    <testSuiteGuid>af622ca4-ab82-4293-a381-7aff9bbb3a1a</testSuiteGuid>
+    <testSuiteGuid>96820378-7e7a-4da6-8d2d-7fb58710ea54</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/xray-test/draft g5 tc</testCaseId>
-        <guid>1186cdb1-48e4-4a77-901e-e50cef37a396</guid>
+        <guid>6e8447b2-eddf-4fbd-9b0c-848785a10b43</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Folder3000/Test Case</testCaseId>
-        <guid>3f7213bb-9bfc-447d-9e1c-c31a0a1fdd7c</guid>
+        <guid>a6ed90d2-2199-4b10-ac1f-4dbd969aaa71</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
-        <guid>8c7c8193-3367-4ff5-b217-a119472a038e</guid>
+        <guid>12d71189-dbc4-49b4-bc80-f757f03bc632</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case 1</testCaseId>
-        <guid>97a57c36-e05d-4bb5-9c4d-52ef76625d25</guid>
+        <guid>ad39269a-eb35-4bb7-b4b8-eec728c84b08</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/Folder1/Folder2/Folder3/Test Case 1</testCaseId>
-        <guid>b39c9004-cda4-4919-889f-a105d7cd934a</guid>
+        <guid>42e80fb0-efde-4697-881d-f926800c41ee</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
-        <guid>e7d9c072-523f-4905-af99-53cc1510ce23</guid>
+        <guid>774ce616-d767-444a-a957-5ad6e3b12446</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/Folder1/New test case</testCaseId>
-        <guid>ec17b9a7-293f-4448-ac07-41490661727a</guid>
+        <guid>8b53c792-33ab-40f4-83c3-a90b54dd4269</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/Folder1/leoleo</testCaseId>
-        <guid>4e9cd3d0-1e8e-484b-b2fc-50be61dd0dfa</guid>
+        <guid>c72b67de-94a9-4f7e-b229-b4f8051c8ffa</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/Folder1/publish without write permission</testCaseId>
-        <guid>c23c955f-11a2-42de-a82e-ead1d50b9699</guid>
+        <guid>a0abff60-614e-4bc2-b040-b9c333ad0abb</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/draft g5 tc</testCaseId>
+        <guid>b7a481bc-d064-4ee9-90e5-367af01c7687</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Folder3000/Test Case</testCaseId>
+        <guid>d553624d-8936-4ddc-9fb8-df43a6cde3c3</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
+        <guid>467bf06b-ec73-4616-8c33-b71fee04fffa</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case 1</testCaseId>
+        <guid>45eb587c-3bc1-4d72-912c-f0c99d31fc08</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Test Case 1</testCaseId>
+        <guid>f67ba399-5275-436e-9e71-667d3f68aae4</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
+        <guid>ef9c1c37-ecb6-4ff4-b1ed-8cf93f142455</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/New test case</testCaseId>
+        <guid>eb4a16e5-0936-4e2e-bcd1-acaa055005ea</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/leoleo</testCaseId>
+        <guid>e0221478-6436-44bc-8aee-2fed2c6a5193</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/publish without write permission</testCaseId>
+        <guid>9653f7c6-a7bb-4195-8224-46c58d2df814</guid>
     </testCaseLink>
 </TestSuiteEntity>
