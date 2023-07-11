e2f05598f2b8c81c2c783087e1bce5e1fc09a7e1
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-07-11 10:08:10 +0700

    first commit

---
+++ test-suites/xray/Cloud Studio Suite 1.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Cloud Studio Suite 1</name>
+    <testSuiteGuid>af622ca4-ab82-4293-a381-7aff9bbb3a1a</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/draft g5 tc</testCaseId>
+        <guid>1186cdb1-48e4-4a77-901e-e50cef37a396</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Folder3000/Test Case</testCaseId>
+        <guid>3f7213bb-9bfc-447d-9e1c-c31a0a1fdd7c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
+        <guid>8c7c8193-3367-4ff5-b217-a119472a038e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case 1</testCaseId>
+        <guid>97a57c36-e05d-4bb5-9c4d-52ef76625d25</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Test Case 1</testCaseId>
+        <guid>b39c9004-cda4-4919-889f-a105d7cd934a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
+        <guid>e7d9c072-523f-4905-af99-53cc1510ce23</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/New test case</testCaseId>
+        <guid>ec17b9a7-293f-4448-ac07-41490661727a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/leoleo</testCaseId>
+        <guid>4e9cd3d0-1e8e-484b-b2fc-50be61dd0dfa</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/publish without write permission</testCaseId>
+        <guid>c23c955f-11a2-42de-a82e-ead1d50b9699</guid>
+    </testCaseLink>
+</TestSuiteEntity>
