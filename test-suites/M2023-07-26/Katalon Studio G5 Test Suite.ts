4044f31f4cebb6455e4606140c3b2279a04edf8a
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-07-24 14:10:56 +0700

    commit

---
+++ test-suites/M2023-07-26/Katalon Studio G5 Test Suite.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Katalon Studio G5 Test Suite</name>
+    <testSuiteGuid>960ec928-b76e-460d-a2be-77ca9c8b2e65</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Folder3000/Test Case</testCaseId>
+        <guid>112b49d0-e466-4c7a-9cd4-90525aab095e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
+        <guid>e3d9f4fe-e647-43b8-b10c-56a8e3fb9d91</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case 1</testCaseId>
+        <guid>911b41af-cf58-485b-a5cf-0e97ff5842c5</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Test Case 1</testCaseId>
+        <guid>be710244-69f2-44fe-a6a3-89daaffabe1b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
+        <guid>503c8c5f-18a7-48df-a2cd-cf1cd7be21d5</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/New test case</testCaseId>
+        <guid>1dc6dd4b-ecd3-4c1d-a2c3-73aea66212d8</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/leoleo</testCaseId>
+        <guid>6007c88d-5202-423c-9ebb-3346cf125b52</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/publish without write permission</testCaseId>
+        <guid>f8276a68-0db8-4348-a411-8ffcd6945b61</guid>
+    </testCaseLink>
+</TestSuiteEntity>
