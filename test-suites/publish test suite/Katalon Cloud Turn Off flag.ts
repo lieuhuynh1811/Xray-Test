1e6c352223df30cc46205b0bc3b89c82f0117e1c
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-07-31 10:01:50 +0700

    first commit

---
+++ test-suites/publish test suite/Katalon Cloud Turn Off flag.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Katalon Cloud Turn Off flag</name>
+    <testSuiteGuid>dfdbf785-9950-4b6f-9a2d-5cc95ff0afcf</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/New test case</testCaseId>
+        <guid>22361cf0-a842-4457-8dce-6eaa6b106a1c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/draft g5 tc</testCaseId>
+        <guid>c77abfc6-d9d0-40a6-88b1-ec0c8332d20a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/M2023-07-26/Katalon Studio G5</testCaseId>
+        <guid>3db4cd33-af3b-497c-a0bf-41645e09a950</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Folder3000/Test Case</testCaseId>
+        <guid>6b449085-a4d8-4abe-a660-dc596cb9693f</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
+        <guid>edd529f3-c4c8-4c8d-ad42-9c25143a2ae1</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case 1</testCaseId>
+        <guid>17b6dc60-2b77-4b30-a402-da8cddedf4cf</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Test Case 1</testCaseId>
+        <guid>b788ffef-e0a4-4882-b30f-ad6945bf4a3b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
+        <guid>9486dc73-c752-4c7a-98dc-485c14c5d9a4</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/New test case</testCaseId>
+        <guid>358e9223-37d0-4221-aa24-c1d8f76715a3</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/leoleo</testCaseId>
+        <guid>4c80b7e4-9227-4405-9040-1afbad95a3b9</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/publish without write permission</testCaseId>
+        <guid>22518767-68bc-4e19-91e0-d244ea649f2e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/api/Test Case</testCaseId>
+        <guid>569ea1e4-be38-42a1-b432-f820e2d90c82</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/g5</testCaseId>
+        <guid>a37f9427-e26f-4460-80e9-d93984a87c2d</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/g5 draft</testCaseId>
+        <guid>10056bce-d267-4b89-ba12-21ca785460f6</guid>
+    </testCaseLink>
+</TestSuiteEntity>
