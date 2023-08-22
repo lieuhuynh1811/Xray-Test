2485ed08814b40f8aad242bf4e1f1595b10990a6
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-08-22 10:30:21 +0700

    commit

---
+++ test-suites/22.08.2023/Cloud Studio Repo.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Cloud Studio Repo</name>
+    <testSuiteGuid>257bafb4-c621-4900-aaba-f57b3c57e341</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Folder3000/Test Case</testCaseId>
+        <guid>42a4d3e9-61c7-4ced-a94e-8a23f3f8ec98</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
+        <guid>215230a2-8f23-4ad1-a26e-9d76439a8eca</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case 1</testCaseId>
+        <guid>3a0231f8-166e-4737-88b8-7b47ce4d2fa7</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Test Case 1</testCaseId>
+        <guid>becafb06-26bc-4332-aa74-0ce617b87f57</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
+        <guid>be9db7ab-6bfa-4792-9d76-759a81ee1bee</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/New test case</testCaseId>
+        <guid>23a150d8-72d2-494f-b7c9-7fcd168ed82a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/leoleo</testCaseId>
+        <guid>65a20fe3-7d47-4088-8969-6e671edb55de</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/22.08.2023/G5 Test Case</testCaseId>
+        <guid>40278b5c-c2b9-423c-9da1-b30a1ba55240</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/g5</testCaseId>
+        <guid>8af6ad9a-b5bc-4476-80d7-9adf99d8d362</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/g5 draft</testCaseId>
+        <guid>f46136f3-047e-4f74-8143-6b768a83c87a</guid>
+    </testCaseLink>
+</TestSuiteEntity>
