a64de2ab3ebc695e68e215fafbc63adf65346175
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-09-20 14:00:39 +0700

    commit

---
+++ test-suites/22.08.2023/G5 Test Suite.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>G5 Test Suite</name>
+    <testSuiteGuid>27cd9b29-8b7f-442e-9065-66280b06f4bb</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/M2023-07-26/Katalon Studio G5</testCaseId>
+        <guid>86afb5f0-bbf7-4644-9bf8-eeba0b6996af</guid>
+    </testCaseLink>
+</TestSuiteEntity>
