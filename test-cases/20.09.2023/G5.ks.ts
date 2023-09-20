786b99f0756694f66b72361f4d2777623d5bc467
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-09-20 11:15:08 +0700

    Katalon: Update test case G5 in test-cases/20.09.2023

--- test-cases/20.09.2023/G5.ks.ts
+++ test-cases/20.09.2023/G5.ks.ts
@@ -24,7 +24,6 @@ export default katalon.testCase("New Test Case", async ({ local, web }) => {
       {
         id: "5570c878-c9c1-412d-9e3b-3a49d73f3b31",
         type: "Attribute",
-        value: null,
         isDefault: true,
       },
     ],
@@ -32,8 +31,35 @@ export default katalon.testCase("New Test Case", async ({ local, web }) => {
     tag: "a",
     text: "Make Appointment",
     pageUrl: "https://katalon-demo-cura.herokuapp.com/",
-    parentIframe: null,
-    shadowRoot: null,
+  });
+  await web.click({
+    type: "Web",
+    id: "23ac69a0-28c3-4f1a-b656-0941e8db2225",
+    attributes: {
+      id: "btn-make-appointment",
+      href: "./profile.php#login",
+      class: "btn btn-dark btn-lg",
+    },
+    childIndex: 4,
+    hashes: { "md5.v1": "3caec008087ccb7a1cb3fc698fbf83c2" },
+    name: "a - Make Appointment",
+    selectors: [
+      {
+        id: "17751c79-b286-4445-aec3-d6fe6fcbde7c",
+        type: "CSS",
+        value: "#btn-make-appointment",
+        isDefault: true,
+      },
+      {
+        id: "5570c878-c9c1-412d-9e3b-3a49d73f3b31",
+        type: "Attribute",
+        isDefault: true,
+      },
+    ],
+    selectorType: "CSS",
+    tag: "a",
+    text: "Make Appointment",
+    pageUrl: "https://katalon-demo-cura.herokuapp.com/",
   });
   await web.closeBrowser();
 });
