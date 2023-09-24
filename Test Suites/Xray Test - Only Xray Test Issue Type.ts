ba536051fccac4abc2d53bba37055e59d6dcd903
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-09-24 13:59:37 +0700

    leo

--- Test Suites/Xray Test - Only Xray Test Issue Type.ts
+++ Test Suites/Xray Test - Only Xray Test Issue Type.ts
@@ -12,13 +12,6 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>638512d9-28d2-48b9-9b23-0448e235ca9b</testSuiteGuid>
    <testCaseLink>
-      <guid>e8f1bf91-72a6-453f-bb61-4b6a46ec6638</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Verify user cannot login</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>6c43af18-81dd-439a-aa71-cf37b7934baa</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
