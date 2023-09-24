438867509f25e80ba8d7e02733d8b817a5e9bb69
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-09-24 15:11:30 +0700

    leo

---
+++ Test Suites/Xray Test - Only Test Issue Type.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Xray Test - Only Test Issue Type</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9f439331-d5a2-40c7-be2e-caafb9cb0250</testSuiteGuid>
+   <testCaseLink>
+      <guid>355fc567-f184-4d12-bf4c-b606c35aa6f4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify user can logout</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
