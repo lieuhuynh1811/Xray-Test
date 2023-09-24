fef71da0ffa292ca48f9f59c9630be567578be08
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-09-24 13:44:00 +0700

    leo

---
+++ test-suites/regression-20062023/Turn off flag git.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Turn off flag git</name>
+    <testSuiteGuid>9a911daa-30a9-4390-a236-7c0be2e5d09a</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/Cleanup flag 27.09</testCaseId>
+        <guid>9d3d22c1-08ff-4385-aee6-e9de3b49dae6</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/Katalon Cloud Turn Off flag</testCaseId>
+        <guid>d34caa66-e9c1-4eab-8f62-c62821a493b1</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/New test case</testCaseId>
+        <guid>96ede4e9-4c42-44d1-b030-2d9e24f0091a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/draft g5 tc</testCaseId>
+        <guid>18ee9123-e80d-40d1-bf3d-b78e086ee9c1</guid>
+    </testCaseLink>
+</TestSuiteEntity>
