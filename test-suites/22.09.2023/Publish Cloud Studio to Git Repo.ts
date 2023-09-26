ba003e0e008826b5b44b3e4a9eaaa0be892e9f9b
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-09-26 15:08:54 +0700

    leoleo

---
+++ test-suites/22.09.2023/Publish Cloud Studio to Git Repo.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Publish Cloud Studio to Git Repo</name>
+    <testSuiteGuid>79e07639-99ae-48bc-8ae0-3a5806231ca5</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/Cleanup flag 27</testCaseId>
+        <guid>55d7d7fe-a7fa-49c9-b007-19c41cd6a9a9</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/Katalon Cloud Turn Off flag</testCaseId>
+        <guid>3d041c5e-cdd2-4568-a3d7-940193f10f8c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/New test case</testCaseId>
+        <guid>5eb0fb7f-f15a-4304-87c9-e0c13fd8d121</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/xray-test/draft g5 tc</testCaseId>
+        <guid>b2046a80-1bae-42f0-95e6-9773da121078</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/new-bae/draft test case</testCaseId>
+        <guid>eca1dc3b-1c3f-4bb8-9bc1-73e4c6825c2f</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/M2023-07-26/Katalon Studio G5</testCaseId>
+        <guid>2f92a997-18e6-45e3-8f7e-6405575c38b4</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/leo-bae/leoleo</testCaseId>
+        <guid>642bb673-a34d-4a69-9df9-90df7db8a98e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Folder3000/Test Case</testCaseId>
+        <guid>de36c1e3-3235-47e9-bc02-c344e461629d</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case</testCaseId>
+        <guid>d8313a2e-8c9b-442b-bf49-7aed432a2dcf</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Folder4/Test Case 1</testCaseId>
+        <guid>e23571c5-d5b9-419e-9e9f-9ad9993edc46</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/Folder3/Test Case 1</testCaseId>
+        <guid>1413fa51-9537-449c-8958-9a2220ea2257</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/Folder2/New Test Case</testCaseId>
+        <guid>f5d097f6-b5e7-4fa9-b415-680ec86fe727</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/New test case</testCaseId>
+        <guid>bdc0a4f6-2bf9-41ff-8406-377c2a06f287</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/leoleo</testCaseId>
+        <guid>70e65c56-a5c6-4d3b-b5b2-eda36946d8bc</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Folder1/publish without write permission</testCaseId>
+        <guid>ec93d98e-baf7-4af3-b57e-1dd02d0d74fc</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/api/Test Case</testCaseId>
+        <guid>19aed557-6368-478a-b98e-dca090a6bb5e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/26.09.2023/Cloud Studio Published</testCaseId>
+        <guid>176d45c6-5966-450b-8869-6bb637d59aac</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/22.08.2023/Cleanup flag 27</testCaseId>
+        <guid>dd4786d9-c1c2-4a3d-88ba-8bb43d5ff64c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/22.08.2023/G5 Test Case</testCaseId>
+        <guid>62ced125-cf4a-46b2-957a-1ad5044161f5</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/20.09.2023/G5</testCaseId>
+        <guid>fa3565af-1b11-4384-a595-2352cbc38c11</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/20.09.2023/G5 Test Case</testCaseId>
+        <guid>44f471f9-0609-4d3d-aead-a32ae8567a20</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/g5</testCaseId>
+        <guid>04621e4a-481b-46c1-b6e9-ccf1cf78f1cd</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/g5 draft</testCaseId>
+        <guid>ec0527a6-75c6-4250-9b22-eb52edea8273</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/leoleo</testCaseId>
+        <guid>064ef332-99e3-4ca6-ac89-0eb404bcd6cc</guid>
+    </testCaseLink>
+</TestSuiteEntity>
