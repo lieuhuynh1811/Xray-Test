6fd30bc32c5abb0028ebac1381c7f701ba7d6849
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-10-22 11:41:02 +0700

    leo

---
+++ test-cases/keycloak/Katalon Cloud G5 Test Case keycloak(GitRepo).ks.ts
@@ -0,0 +1,39 @@
+import katalon from "@katalon-studio/cloud-test";
+
+export default katalon.testCase("New Test Case", async ({ local, web }) => {
+  await new Promise((r) => setTimeout(r, 0));
+  await web.openBrowser("https://katalon-demo-cura.herokuapp.com");
+  await web.click({
+    type: "Web",
+    id: "c7fe10a4-5839-4ac0-94a5-67fdb53fb496",
+    attributes: {
+      id: "btn-make-appointment",
+      href: "./profile.php#login",
+      class: "btn btn-dark btn-lg",
+    },
+    childIndex: 4,
+    hashes: { "md5.v1": "3caec008087ccb7a1cb3fc698fbf83c2" },
+    name: "a - Make Appointment",
+    selectors: [
+      {
+        id: "fba79eca-d69a-4919-be97-7222a10eca02",
+        type: "CSS",
+        value: "#btn-make-appointment",
+        isDefault: true,
+      },
+      {
+        id: "7b5e3420-2a36-40a3-b8bc-a90036b9b06d",
+        type: "Attribute",
+        value: null,
+        isDefault: true,
+      },
+    ],
+    selectorType: "CSS",
+    tag: "a",
+    text: "Make Appointment",
+    pageUrl: "https://katalon-demo-cura.herokuapp.com/",
+    parentIframe: null,
+    shadowRoot: null,
+  });
+  await web.closeBrowser();
+});
